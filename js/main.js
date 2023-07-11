aff513e3e2e282127809c6d52504088da1f05054
Ksenia Lavrukhina <burkamera@gmail.com>
2023-07-12 00:28:45 +0300

    Добавляет эффекты

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import { createPosts } from './data.js';
-import { renderData } from './fullSizePicture.js';
 import './form.js';
 import './photo-scale.js';
+import './photo-effects.js';
+import { createPosts } from './data.js';
+import { renderData } from './fullSizePicture.js';
 
 renderData(createPosts());
